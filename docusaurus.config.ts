3522aa120afe9c0eba6536c9abcc24c2cd77d1dd
hamdownnn <luke@m87.ai>
2025-09-01 14:35:35 -0700

    updated url to match new repo

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -15,10 +15,10 @@ const config: Config = {
   },
 
   // Set the production url of your site here
-  url: 'https://lumal-code.github.io/',
+  url: 'ideal-telegram-e2nrqvz.pages.github.io',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/md-docs/',
+  baseUrl: '/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
